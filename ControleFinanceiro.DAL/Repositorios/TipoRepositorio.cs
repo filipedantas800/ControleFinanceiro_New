58ad577a9fd26787f5853ee43444f481d4667091
filipedantas800 <filipetecnico800@hotmail.com>
2023-07-19 18:03:23 -0300

    Repositório de categorias

---
+++ ControleFinanceiro.DAL/Repositorios/TipoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleFinanceiro.DAL.Repositorios
+{
+    public class TipoRepositorio : RepositorioGenerico<Tipo>, ITipoRepositorio
+    {
+        public TipoRepositorio(Contexto contexto) : base(contexto)
+        {
+        }
+    }
+}
